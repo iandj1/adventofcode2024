37160014667c0de736f747cc910fd47156fc4b41
Ian de Jongh <57132463+iandj1@users.noreply.github.com>
2024-12-09 23:47:40 +1100

    clean up day 9 part 2

--- 09p2.rb
+++ 09p2.rb
@@ -1,36 +1,36 @@
 input = File.open("inputs/9-sample.txt")
 input = File.open("inputs/9.txt")
 
-files = {}
+files = []
 spaces = []
 
 index = 0
-input.readline.chars.each_slice(2).with_index do |(data, space), num|
-  files[num] = [index, data.to_i] # file_no => [location, length]
+input.readline.chars.each_slice(2) do |data, space|
+  files << [index, data.to_i]
   spaces << [index + data.to_i, space.to_i] if space && space.to_i > 0
   index += data.to_i + space.to_i
 end
 
-files.keys.reverse.each do |file|
-  file_index = files[file][0]
-  file_length = files[file][1]
+files.reverse_each do |file|
+  file_index = file[0]
+  file_length = file[1]
 
   spaces.each_with_index do |(space_index, space_length), space_no|
     break if space_index > file_index # never move file right
     if file_length == space_length
       spaces.delete_at(space_no)
-      files[file][0] = space_index
+      file[0] = space_index
       break
     elsif file_length < space_length
       spaces[space_no] = [space_index + file_length, space_length - file_length]
-      files[file][0] = space_index
+      file[0] = space_index
       break
     end
   end
 end
 
 total = 0
-files.each do |file_num, (location, length)|
+files.each_with_index do |(location, length), file_num|
   (location...location+length).each do |index|
     total += index * file_num
   end
