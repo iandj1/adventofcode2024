5f25ebe5109431a30609f614227ab75b05d01e65
iandj1 <57132463+iandj1@users.noreply.github.com>
2024-12-19 18:16:50 +1100

    compact day 19 part 2

--- 19p2.rb
+++ 19p2.rb
@@ -3,27 +3,14 @@ input = File.open("inputs/19.txt")
 
 towels = input.readline.scan(/[a-z]+/)
 input.readline
-
-designs = []
-input.each do |line|
-  designs << line.strip
-end
+designs = input.map(&:strip)
 
 def possible_count(towels, design, memo)
-  if !memo[design]
-    if design == ''
-      memo[design] = 1
-    else
-      memo[design] = towels.sum do |towel|
-        if design.start_with? towel
-          possible_count(towels, design.delete_prefix(towel), memo)
-        else
-          0
-        end
-      end
-    end
+  return 1 if design == ''
+  memo[design] ||= towels.sum do |towel|
+    next 0 unless design.start_with? towel
+    possible_count(towels, design.delete_prefix(towel), memo)
   end
-  memo[design]
 end
 
 puts designs.sum{|design| possible_count(towels, design, {})}
