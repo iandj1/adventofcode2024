95c50ca43e01690efd263c128166c40fe5353463
iandj1 <57132463+iandj1@users.noreply.github.com>
2024-12-29 13:38:52 +0000

    day 24 part 2

---
+++ 24p2.rb
@@ -0,0 +1,65 @@
+input = File.open("inputs/24.txt")
+
+# skip wires
+input.each do |line|
+  break if line.strip.empty?
+end
+
+gates =  {}
+# parse gates
+input.each do |line|
+  line = line.strip.split(' ')
+  gates[line[4]] = [line[0], line[1], line[2]] # in1, op, in2
+end
+
+def logic_str(gates, wire, swaps={}) # generate logic string for node recursively to exclude intermediate nodes
+  wire = swaps[wire] || wire # perform any swaps specified
+  return wire if ['x','y'].include? wire[0] # input wire. return wire name
+  in1, op, in2 = gates[wire]
+  str1 = logic_str(gates, in1, swaps)
+  str2 = logic_str(gates, in2, swaps)
+  str1, str2 = [str1, str2].sort
+  return "(#{str1} #{op} #{str2})"
+end
+
+def build_adder(gates, n) # construct n-bit adder circuit for comparison
+  n_str = '%02d' % n
+  n_str_sub = '%02d' % (n-1)
+  if n == 0 # half adder
+    gates["z#{n_str}"] = ["x#{n_str}", "XOR", "y#{n_str}"]
+    gates["carry#{n_str}"] = ["x#{n_str}", "AND", "y#{n_str}"]
+  else # full adder
+    gates["exor_xy#{n_str}"] = ["x#{n_str}", "XOR", "y#{n_str}"]
+    gates["z#{n_str}"] = ["exor_xy#{n_str}", "XOR", "carry#{n_str_sub}"]
+    gates["and_carry1#{n_str}"] = ["carry#{n_str_sub}", "AND", "exor_xy#{n_str}"]
+    gates["and_carry2#{n_str}"] = ["x#{n_str}", "AND", "y#{n_str}"]
+    gates["carry#{n_str}"] = ["and_carry1#{n_str}", "OR", "and_carry2#{n_str}"]
+    build_adder(gates, n-1)
+  end
+end
+
+correct_gates = {}
+build_adder(correct_gates, 44) # build working 44 bit adder for comparison
+
+known_swaps = []
+
+(0..44).each do |n|
+  puts n
+  gate = 'z%02d' % n
+  swaps = (known_swaps + known_swaps.map(&:reverse)).to_h
+  if logic_str(gates, gate, swaps) != logic_str(correct_gates, gate) # logic error found
+    gates.keys.permutation(2).each do |gate1, gate2|
+      new_swaps = known_swaps + [[gate1, gate2]]
+      swaps = (new_swaps + new_swaps.map(&:reverse)).to_h
+      begin
+        if logic_str(gates, gate, swaps) == logic_str(correct_gates, gate) # error fixed
+          known_swaps << [gate1, gate2]
+          break
+        end
+      rescue SystemStackError # stuck in a loop. Move on.
+      end
+    end
+  end
+end
+
+puts known_swaps.flatten.sort.join(',')
