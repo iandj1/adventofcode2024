213a9d7e093abaf1fcd675e447c62e10ef947b00
Ian de Jongh <57132463+iandj1@users.noreply.github.com>
2024-12-30 13:04:22 +1100

    improve day 24 performance

--- 24p2.rb
+++ 24p2.rb
@@ -12,12 +12,13 @@ input.each do |line|
   gates[line[4]] = [line[0], line[1], line[2]] # in1, op, in2
 end
 
-def logic_str(gates, wire, swaps={}) # generate logic string for node recursively to exclude intermediate nodes
+def logic_str(gates, wire, swaps={}, visited_wires=[]) # generate logic string for node recursively to exclude intermediate nodes
   wire = swaps[wire] || wire # perform any swaps specified
+  visited_wires << wire
   return wire if ['x','y'].include? wire[0] # input wire. return wire name
   in1, op, in2 = gates[wire]
-  str1 = logic_str(gates, in1, swaps)
-  str2 = logic_str(gates, in2, swaps)
+  str1 = logic_str(gates, in1, swaps, visited_wires)
+  str2 = logic_str(gates, in2, swaps, visited_wires)
   str1, str2 = [str1, str2].sort
   return "(#{str1} #{op} #{str2})"
 end
@@ -43,16 +44,20 @@ build_adder(correct_gates, 44) # build working 44 bit adder for comparison
 
 known_swaps = []
 
+gates_to_permute = gates.keys.to_set
+
 (0..44).each do |n|
-  puts n
   gate = 'z%02d' % n
   swaps = (known_swaps + known_swaps.map(&:reverse)).to_h
-  if logic_str(gates, gate, swaps) != logic_str(correct_gates, gate) # logic error found
-    gates.keys.permutation(2).each do |gate1, gate2|
+  tested_gates = []
+  correct_str = logic_str(correct_gates, gate)
+  if logic_str(gates, gate, swaps, tested_gates) != correct_str # logic error found
+    gates_to_permute.to_a.permutation(2).each do |gate1, gate2|
       new_swaps = known_swaps + [[gate1, gate2]]
       swaps = (new_swaps + new_swaps.map(&:reverse)).to_h
+      tested_gates = []
       begin
-        if logic_str(gates, gate, swaps) == logic_str(correct_gates, gate) # error fixed
+        if logic_str(gates, gate, swaps, tested_gates) == correct_str # error fixed
           known_swaps << [gate1, gate2]
           break
         end
@@ -60,6 +65,7 @@ known_swaps = []
       end
     end
   end
+  gates_to_permute.subtract(tested_gates)
 end
 
 puts known_swaps.flatten.sort.join(',')
