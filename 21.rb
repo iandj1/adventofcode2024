c0b57267fd0dcdda459656cb4c6f2743afdec8b7
Ian de Jongh <57132463+iandj1@users.noreply.github.com>
2025-01-07 23:59:25 +1100

    improve day 21 part 1

--- 21.rb
+++ 21.rb
@@ -39,32 +39,33 @@ def build_shortest_path_hash(pad_hash, pad)
     to_row, to_col = pad_hash[to]
     if from_row < to_row
       vertical_str = 'v' * (to_row - from_row)
-      proposed_rows = (from_row..to_row)
     elsif from_row > to_row
       vertical_str = '^' * (from_row - to_row)
-      proposed_rows = (to_row..from_row)
     else
       vertical_str = ''
-      proposed_rows = [to_row]
     end
     if from_col < to_col
       horizontal_str = '>' * (to_col - from_col)
-      proposed_cols = (from_col..to_col)
     elsif from_col > to_col
       horizontal_str = '<' * (from_col - to_col)
-      proposed_cols = (to_col..from_col)
     else
       horizontal_str = ''
-      proposed_cols = [to_col]
     end
-    path_hash[[from, to]] = []
-    if proposed_cols.all?{|col| pad[from_row][col]}
-      path_hash[[from, to]] << horizontal_str + vertical_str + 'A'
+    options = []
+    if pad[from_row][to_col]
+      options << horizontal_str + vertical_str + 'A'
     end
-    if proposed_rows.all?{|row| pad[row][from_col]}
-      path_hash[[from, to]] << vertical_str + horizontal_str + 'A'
+    if pad[to_row][from_col]
+      options << vertical_str + horizontal_str + 'A'
     end
-    path_hash[[from, to]].uniq!
+    options.uniq!
+    if options.count == 1
+      path_hash[[from, to]] = options[0]
+    elsif ['v>', '<v', '<^'].any?{|x| options[0].include? x} # these directions are better than alternatives
+      path_hash[[from, to]] = options[0]
+    else
+      path_hash[[from, to]] = options[1]
+    end
   end
   path_hash
 end
@@ -73,25 +74,17 @@ shortest_num_paths = build_shortest_path_hash(num_hash, num_pad)
 shortest_arrow_paths = build_shortest_path_hash(arrow_hash, arrow_pad)
 
 def possible_paths(str, path, shortest_paths_hash)
-  return [path] if str.length == 1
-  next_segments = shortest_paths_hash[[str[0],str[1]]] || ['A']
-  paths = []
-  next_segments.each do |segment|
-    paths << possible_paths(str[1..], path + segment, shortest_paths_hash)
-  end
-  return paths.flatten
-end
-
-def keep_shortest(paths)
-  length = paths.map(&:length).min
-  paths.select {|path| path.length == length}
+  return path if str.length == 1
+  next_segment = shortest_paths_hash[[str[0],str[1]]] || 'A'
+  possible_paths(str[1..], path + next_segment, shortest_paths_hash)
 end
 
 shortest_overall_lengths = {}
-shortest_num_paths.each do |(from,to), paths|
-  layer_2 = keep_shortest(paths.map{ |path| possible_paths('A' + path, '', shortest_arrow_paths)}).flatten
-  layer_3 = keep_shortest(layer_2.map{ |path| possible_paths('A' + path, '', shortest_arrow_paths)}).flatten
-  shortest_overall_lengths[[from, to]] = layer_3[0].length
+shortest_num_paths.each do |step, path|
+  2.times do
+    path = possible_paths('A' + path, '', shortest_arrow_paths)
+  end
+  shortest_overall_lengths[step] = path.length
 end
 
 total = 0
