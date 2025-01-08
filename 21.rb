fb005db1548b4ebd7f90ace4022d2e2b3a54cc43
Ian de Jongh <57132463+iandj1@users.noreply.github.com>
2025-01-09 00:31:09 +1100

    improve day 21

--- 21.rb
+++ 21.rb
@@ -33,23 +33,11 @@ arrow_hash = build_pad_hash(arrow_pad)
 
 def build_shortest_path_hash(pad_hash, pad)
   path_hash = {}
-  pad_hash.keys.permutation(2).each do |from, to|
+  pad_hash.keys.product(pad_hash.keys).each do |from, to|
     from_row, from_col = pad_hash[from]
     to_row, to_col = pad_hash[to]
-    if from_row < to_row
-      vertical_str = 'v' * (to_row - from_row)
-    elsif from_row > to_row
-      vertical_str = '^' * (from_row - to_row)
-    else
-      vertical_str = ''
-    end
-    if from_col < to_col
-      horizontal_str = '>' * (to_col - from_col)
-    elsif from_col > to_col
-      horizontal_str = '<' * (from_col - to_col)
-    else
-      horizontal_str = ''
-    end
+    vertical_str = (from_row < to_row ? 'v' : '^') * (from_row - to_row).abs
+    horizontal_str = (from_col < to_col ? '>' : '<') * (from_col - to_col).abs
     options = []
     if pad[from_row][to_col]
       options << horizontal_str + vertical_str + 'A'
@@ -72,22 +60,16 @@ end
 shortest_num_paths = build_shortest_path_hash(num_hash, num_pad)
 shortest_arrow_paths = build_shortest_path_hash(arrow_hash, arrow_pad)
 
-def new_path(str, path, shortest_paths_hash)
-  return path if str.length == 1
-  next_segment = shortest_paths_hash[[str[0],str[1]]] || 'A'
-  new_path(str[1..], path + next_segment, shortest_paths_hash)
+def new_path_arr(str, path_hash)
+  ('A' + str).chars.each_cons(2).map{|pair| path_hash[pair]} # split into pairs, assuming we start on A
 end
 
 def path_len(str, path_hash, n, memo)
   key = [str, n]
-  return memo[key] if memo[key] # return if we've already seen this
-  if str == 'A'
-    memo[key] = 1
-  else
-    substrings = str.split('A')
-    new_paths = substrings.map{|str| new_path('A' + str + 'A', '', path_hash)}
+  if !memo[key] # return if we've already seen this
+    new_paths = new_path_arr(str, path_hash)
     if n == 1
-      memo[key] = new_paths.sum{|path| path.length}
+      memo[key] = new_paths.sum(&:length)
     else
       memo[key] = new_paths.sum{|path| path_len(path, path_hash, n-1, memo)}
     end
@@ -99,7 +81,7 @@ memo = {}
 total_1 = 0
 total_2 = 0
 codes.each do |code|
-  path = new_path('A' + code, '', shortest_num_paths)
+  path = new_path_arr(code, shortest_num_paths).join
   total_1 += code.to_i * path_len(path, shortest_arrow_paths, 2, memo)
   total_2 += code.to_i * path_len(path, shortest_arrow_paths, 25, memo)
 end
