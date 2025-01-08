1ceffe338005aa0794a23092f6a8ce8a0c636be1
iandj1 <57132463+iandj1@users.noreply.github.com>
2025-01-08 16:42:42 +1100

    day 21 part 2

--- 21.rb
+++ 21.rb
@@ -2,7 +2,6 @@ input = File.open("inputs/21-sample.txt")
 input = File.open("inputs/21.txt")
 
 codes = []
-
 input.each do |line|
   codes << line.strip
 end
@@ -73,24 +72,36 @@ end
 shortest_num_paths = build_shortest_path_hash(num_hash, num_pad)
 shortest_arrow_paths = build_shortest_path_hash(arrow_hash, arrow_pad)
 
-def possible_paths(str, path, shortest_paths_hash)
+def new_path(str, path, shortest_paths_hash)
   return path if str.length == 1
   next_segment = shortest_paths_hash[[str[0],str[1]]] || 'A'
-  possible_paths(str[1..], path + next_segment, shortest_paths_hash)
+  new_path(str[1..], path + next_segment, shortest_paths_hash)
 end
 
-shortest_overall_lengths = {}
-shortest_num_paths.each do |step, path|
-  2.times do
-    path = possible_paths('A' + path, '', shortest_arrow_paths)
+def path_len(str, path_hash, n, memo)
+  key = [str, n]
+  return memo[key] if memo[key] # return if we've already seen this
+  if str == 'A'
+    memo[key] = 1
+  else
+    substrings = str.split('A')
+    new_paths = substrings.map{|str| new_path('A' + str + 'A', '', path_hash)}
+    if n == 1
+      memo[key] = new_paths.sum{|path| path.length}
+    else
+      memo[key] = new_paths.sum{|path| path_len(path, path_hash, n-1, memo)}
+    end
   end
-  shortest_overall_lengths[step] = path.length
+  memo[key]
 end
 
-total = 0
+memo = {}
+total_1 = 0
+total_2 = 0
 codes.each do |code|
-  length = ('A' + code).chars.each_cons(2).sum{|segment| shortest_overall_lengths[segment]}
-  total += code.to_i * length
+  path = new_path('A' + code, '', shortest_num_paths)
+  total_1 += code.to_i * path_len(path, shortest_arrow_paths, 2, memo)
+  total_2 += code.to_i * path_len(path, shortest_arrow_paths, 25, memo)
 end
-
-puts total
+puts total_1
+puts total_2
